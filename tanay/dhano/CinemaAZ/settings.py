8c3cb5d4c413c9555087cd7c1197a8d52343cb70
Tanay <210050155@iitb.ac.in>
2022-11-23 02:54:10 +0530

    User registration

--- tanay/dhano/CinemaAZ/settings.py
+++ tanay/dhano/CinemaAZ/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webscraper.apps.WebscraperConfig',
+    'register.apps.RegisterConfig',
+    
 ]
 
 MIDDLEWARE = [
