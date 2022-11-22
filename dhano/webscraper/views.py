84e1f3606319563d98aaa4d80e65f959ca541f3a
DhanoHacks <draman9224@gmail.com>
2022-11-22 05:46:41 +0530

    made functioning movie page

--- dhano/webscraper/views.py
+++ dhano/webscraper/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import MovieData
 
 def index(response):
-    return HttpResponse("Dhano was here!")
+    m = MovieData.objects.get(id=1)
+    return render(response, "webscraper/movie.html", {"m":m})
